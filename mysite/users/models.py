c0c68640c5e0b0057f8cd21ede0f4e29a6eda697
Yashar <ya333har@gmail.com>
2026-06-12 01:27:03 +0330

    first commit

---
+++ mysite/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
